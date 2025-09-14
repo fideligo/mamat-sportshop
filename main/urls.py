36ff1b0a11d60fd6b9d69c58b1a773a3ac5632fa
Fideligo <mrafighalibf@gmail.com>
2025-09-14 16:11:53 +0700

    feat: routing for the new feats

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,8 @@ from .views import main_page
 
 urlpatterns = [
     path('', main_page, name='main-page'),
+    path('create-product/', create_product, name='create_product'),
+    path('product/<str:id>/', show_product, name='show_product'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
 ]
