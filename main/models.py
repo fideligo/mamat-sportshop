45931cfcd47be804826481670572a2fb724cf2b6
Fideligo <mrafighalibf@gmail.com>
2025-09-21 11:34:30 +0700

    feat: tambahin atribut user ke product

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     CATEGORY_CHOICES = [
@@ -11,6 +12,7 @@ class Product(models.Model):
     ]
 
     # mandatory fields
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     price = models.IntegerField(default=0) 
